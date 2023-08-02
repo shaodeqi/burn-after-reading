c6bbae19cfae400d0e1f80460e48c32dd16364db
deqishao <deqishao@tencent.com>
2023-08-02 12:31:33 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     vue({ 
       template: { transformAssetUrls }
