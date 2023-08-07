223b6e9ea7abb77c8f92493fbb2f4c37d5b76617
deqishao <deqishao@tencent.com>
2023-08-07 10:29:34 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,139 @@
 <template>
-  <div class="d-flex flex-column fill-height" :style="{
-    filter: connected ? 'unset' : 'blur(10px)',
-  }">
+  <div
+    class="d-flex flex-column fill-height"
+    :style="{
+      filter: connected ? 'unset' : 'blur(10px)',
+    }"
+  >
     <!-- <div class="d-flex align-center justify-center px-1 py-2 header-container">
       <span>1</span>
       <span style="position: absolute; right: 12px">设置</span>
     </div> -->
-    <div class="network-status position-absolute" :class="NETWORK_STATUS[networkState]">
+    <div
+      class="network-status position-absolute"
+      :class="NETWORK_STATUS[networkState]"
+    >
       <svg width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
         <path
-          d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.444 12.444 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049z" />
+          d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.444 12.444 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049z"
+        />
         <path
-          d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.455 9.455 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065zm-2.183 2.183c.226-.226.185-.605-.1-.75A6.473 6.473 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.478 5.478 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091l.016-.015zM9.06 12.44c.196-.196.198-.52-.04-.66A1.99 1.99 0 0 0 8 11.5a1.99 1.99 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z" />
+          d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.455 9.455 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065zm-2.183 2.183c.226-.226.185-.605-.1-.75A6.473 6.473 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.478 5.478 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091l.016-.015zM9.06 12.44c.196-.196.198-.52-.04-.66A1.99 1.99 0 0 0 8 11.5a1.99 1.99 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z"
+        />
       </svg>
     </div>
     <v-divider></v-divider>
-    <div class="d-flex flex-column flex-1-1 pa-3 overflow-auto dialogs-container" ref="dialogsContainer">
+    <div
+      class="d-flex flex-column flex-1-1 pa-3 overflow-auto dialogs-container"
+      ref="dialogsContainer"
+    >
       <TransitionGroup name="dialog">
         <template v-for="(dialog, dIndex) in dialogs" :key="dialog.id">
-          <div v-if="dialog.type === 'text'" :class="[own(dialog) ? 'align-self-end' : 'align-self-start']"
-            style="max-width: 90%" @click="handleRead(dialog, dIndex)">
-            <div class="text-medium-emphasis text-body-2" :class="[own(dialog) ? 'text-right' : 'text-left']">
-              <span v-if="dialog.timestamp && own(dialog)" class="text-body-2 text-grey-lighten-1">{{ new
-                Date(dialog.timestamp).toLocaleString() }}</span>
+          <div
+            v-if="dialog.type === 'text'"
+            :class="[own(dialog) ? 'align-self-end' : 'align-self-start']"
+            style="max-width: 90%"
+            @click="handleRead(dialog, dIndex)"
+          >
+            <div
+              class="text-medium-emphasis text-body-2"
+              :class="[own(dialog) ? 'text-right' : 'text-left']"
+            >
+              <span
+                v-if="dialog.timestamp && own(dialog)"
+                class="text-body-2 text-grey-lighten-1"
+                >{{ new Date(dialog.timestamp).toLocaleString() }}</span
+              >
               {{ dialog.nick }}
-              <span v-if="dialog.timestamp && !own(dialog)" class="text-body-2 text-grey-lighten-1">{{ new
-                Date(dialog.timestamp).toLocaleString() }}</span>
+              <span
+                v-if="dialog.timestamp && !own(dialog)"
+                class="text-body-2 text-grey-lighten-1"
+                >{{ new Date(dialog.timestamp).toLocaleString() }}</span
+              >
             </div>
-            <v-badge color="red-lighten-1" :model-value="badge(dialog)" :content="dialog.countDown" :dot="dot(dialog)"
-              :location="own(dialog) ? 'top start' : 'top end'" :class="[own(dialog) ? 'float-right own' : 'float-left']"
-              class="mb-4 px-1 rounded dialog-badge">
-              <div style="white-space: break-spaces" class="px-1 py-2 dialog" :class="{
-                blur: blur(dialog),
-              }">
+            <v-badge
+              color="red-lighten-1"
+              :model-value="badge(dialog)"
+              :content="dialog.countDown"
+              :dot="dot(dialog)"
+              :location="own(dialog) ? 'top start' : 'top end'"
+              :class="[own(dialog) ? 'float-right own' : 'float-left']"
+              class="mb-4 px-1 rounded dialog-badge"
+            >
+              <div
+                style="white-space: break-spaces"
+                class="px-1 py-2 dialog"
+                :class="{
+                  blur: blur(dialog),
+                }"
+              >
                 {{ dialog.message }}
               </div>
             </v-badge>
           </div>
           <div v-if="dialog.type === 'state.enter'" class="align-self-center">
-            <span class="text-body-2 text-grey-darken-1">{{ dialog.message }}进入房间</span>
+            <span class="text-body-2 text-grey-darken-1"
+              >{{ dialog.message }}进入房间</span
+            >
           </div>
           <div v-if="dialog.type === 'state.leave'" class="align-self-center">
-            <span class="text-body-2 text-grey-darken-1">{{ dialog.message }}离开房间</span>
+            <span class="text-body-2 text-grey-darken-1"
+              >{{ dialog.message }}离开房间</span
+            >
           </div>
         </template>
       </TransitionGroup>
     </div>
     <v-divider></v-divider>
     <div class="d-flex px-4 py-2 pb-5 input-container bg-grey-lighten-4">
-      <v-textarea @focus="scrollToBottom()" @input="scrollToBottom()" @keydown.enter="handleEnter" :autofocus="true"
-        base-color="white" bg-color="white" theme="white" maxlength="300" enterkeyhint="send" v-model="message" auto-grow
-        variant="outlined" hide-details rows="1"></v-textarea>
+      <v-textarea
+        @focus="scrollToBottom()"
+        @input="scrollToBottom()"
+        @keydown.enter="handleEnter"
+        :autofocus="true"
+        base-color="white"
+        bg-color="white"
+        theme="white"
+        maxlength="300"
+        enterkeyhint="send"
+        v-model="message"
+        auto-grow
+        variant="outlined"
+        hide-details
+        rows="1"
+      ></v-textarea>
     </div>
   </div>
-  <div class="d-flex flex-column align-center fill-height w-100" style="position: absolute" v-if="!connected">
+  <div
+    class="d-flex flex-column align-center fill-height w-100"
+    style="position: absolute"
+    v-if="!connected"
+  >
     <div class="w-75">
-      <!-- <div class="mt-2" v-if="inWeiXin">
-        <v-alert
-          class="text-body-2"
-          position="fixed"
-          location="top right"
-          variant="text"
-        >
-          进入 <span>123</span> 模式切换页面体验更佳 ⇧
-        </v-alert>
-      </div> -->
-      <v-text-field class="mt-16" :loading="loading.nick" label="请输入昵称后 ↵ 进入" v-model="nick" enterkeyhint="go"
-        @keydown.enter="handleNick"></v-text-field>
+      <v-text-field
+        class="mt-16"
+        :loading="loading.nick"
+        label="请输入昵称后 ↵ 进入"
+        v-model="nick"
+        enterkeyhint="go"
+        @keydown.enter="handleNick"
+      ></v-text-field>
     </div>
   </div>
   <v-overlay v-model="overlay" contained class="align-center justify-center">
-    <v-window :model-value="true" show-arrows style="width: 80vw; max-width: 80vh;">
+    <v-window
+      :model-value="true"
+      show-arrows
+      style="width: 80vw; max-width: 80vh"
+    >
       <v-window-item>
         <v-card class="d-flex flex-column justify-center align-center py-10">
           <div class="pb-10">点击消息进行阅读，倒计时完成时销毁</div>
-          <img style="width: 50vw; min-height: 108vw" src="./assets/guide.webp" alt="使用说明" />
+          <img
+            style="width: 50vw; min-height: 108vw"
+            src="./assets/guide.webp"
+            alt="使用说明"
+          />
         </v-card>
       </v-window-item>
       <v-window-item>
@@ -82,21 +141,31 @@
           <div class="pb-10">
             开启<span class="font-weight-black">浮窗</span>模式体验更佳
           </div>
-          <img style="width: 50vw; min-height: 108vw" src="./assets/float.webp" alt="浮窗模式" />
+          <img
+            style="width: 50vw; min-height: 108vw"
+            src="./assets/float.webp"
+            alt="浮窗模式"
+          />
         </v-card>
       </v-window-item>
     </v-window>
   </v-overlay>
-  <v-snackbar location="top" color="red-lighten-1" timeout="3000" v-model="snackbar.visible">{{ snackbar.content
-  }}</v-snackbar>
-  <img width="0" height="0" src="./assets/guide.webp" />
-  <img width="0" height="0" src="./assets/float.webp" />
+  <v-snackbar
+    location="top"
+    color="red-lighten-1"
+    timeout="3000"
+    v-model="snackbar.visible"
+    >{{ snackbar.content }}</v-snackbar
+  >
+  <!-- <img width="0" height="0" src="./assets/guide.webp" />
+  <img width="0" height="0" src="./assets/float.webp" /> -->
 </template>
 
 <script setup>
 import { ref, reactive, onMounted, nextTick, computed } from "vue";
 import md5 from "blueimp-md5";
 import {
+  notify,
   dialogState,
   randomString,
   handleMessageData,
@@ -106,7 +175,7 @@ import {
   NETWORK_STATUS,
 } from "@/utils";
 import listenVisualViewport from "@/compositions/visual-viewport";
-import online from '@/compositions/network';
+import online from "@/compositions/network";
 
 import ReconnectingWebSocket from "reconnecting-websocket";
 
@@ -185,7 +254,6 @@ const handleEnter = (event) => {
     state: dialogState.UNREAD,
     countDown: 0,
     id: randomString(16),
-    timestamp: +new Date(),
   });
 
   event.preventDefault();
@@ -240,8 +308,10 @@ const connect = () => {
           dialogs.value.pop();
         }
 
-        if (dialogs.value[dialogs.value.length - 1]?.type === "state.enter"
-          && dialogs.value[dialogs.value.length - 1]?.message === user) {
+        if (
+          dialogs.value[dialogs.value.length - 1]?.type === "state.enter" &&
+          dialogs.value[dialogs.value.length - 1]?.message === user
+        ) {
           return;
         }
 
@@ -265,13 +335,21 @@ const connect = () => {
         switch (data?.type) {
           case "message.get":
             if (data.content instanceof Array) {
-              dialogs.value = [dialogs.value.filter(d => d.countDown > 0), ...data.content];
+              dialogs.value = [
+                dialogs.value.filter((d) => d.countDown > 0),
+                ...data.content,
+              ];
             }
             scrollToBottom();
             break;
 
           case "message.new":
             dialogs.value.push({ ...data.content, timestamp });
+
+            if (data.content.nick && data.content.nick !== nick.value) {
+              notify(room, data.content.message);
+            }
+
             scrollToBottom();
             break;
 
@@ -333,7 +411,7 @@ const dot = (dialog) => dialog.state === dialogState.UNREAD;
 const badge = (dialog) => dialog.state !== dialogState.READ;
 const blur = (dialog) => !own(dialog) && dialog.state === dialogState.UNREAD;
 const networkState = computed(() =>
-  online.value ? connectState.value : ReconnectingWebSocket.CLOSED,
+  online.value ? connectState.value : ReconnectingWebSocket.CLOSED
 );
 
 onMounted(() => {
@@ -358,7 +436,7 @@ body,
   &.offline {
     color: #f56c6c;
     &::after {
-      content: '╲';
+      content: "╲";
       position: absolute;
       left: 0;
       bottom: 0;
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,32 @@ export * from "./constants"
 export const type = (content) =>
   Object.prototype.toString.call(content).slice(8, -1);
 
+export const notify = (title, body = '') => {
+if (!('Notification' in globalThis)) {
+    return;
+}
+const _notify = () => {
+    new Notification(title, {
+    body,
+    vibrate: true,
+    icon: '/meeting.svg',
+    }).onclick = () => {
+    globalThis.focus();
+    };
+};
+
+if (Notification.permission === 'granted') {
+    _notify();
+    globalThis.focus();
+} else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+        _notify();
+    }
+    });
+}
+};
+
 export const handleMessageData = async (data) => {
   let payloadStr = data;
   if (type(data) === 'Blob') {
